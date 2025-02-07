07a177ff87901d5ece9abff3fb0a34443c1cf6ad
langaritaivan <langaritalatorre24@iespabloserrano.com>
2025-02-07 10:03:52 +0100

    hoy

---
+++ JS/Ejer_scrip/js.js
@@ -0,0 +1,13 @@
+function calcularCirculo(Radio){
+  const PI=match.PI;
+  const area=PI*Match.pow(radio,2);
+  const longitud=2*PI*radio;
+
+  return{
+    area:area,
+    longitud: longitud
+  };
+}
+
+const radio=5;
+const resultado=calcularCirculo(radio);
